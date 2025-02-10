42a3b8d7a7d57bfdbcac18a83e6456c4345d226f
crypticGøøse <christogoosen@gmail.com>
2025-02-10 11:25:43 +0200

    Create test_sample_data_cleaning.py

---
+++ tests/test_sample_data_cleaning.py
@@ -0,0 +1,14 @@
+# sample_data_cleaning.py
+def cleanse_data(df):
+    return df.dropna()
+
+# test_sample_data_cleaning.py
+import pytest
+import pandas as pd
+from sample_data_cleaning import cleanse_data
+
+def test_cleanse_data():
+    sample_data = pd.DataFrame({'col1': [1, None, 3]})
+    expected_data = pd.DataFrame({'col1': [1, 3]})
+    cleansed_data = cleanse_data(sample_data)
+    pd.testing.assert_frame_equal(cleansed_data, expected_data)
